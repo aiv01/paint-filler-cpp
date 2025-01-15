cd4362d2082a3780cc2c945c024640aafde4ac20
Federico De Felici <fdf8210@gmail.com>
2025-01-15 17:02:14 +0100

    add check for pixel already visited

--- PaintFillerCpp.cpp
+++ PaintFillerCpp.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include "Image.h"
+#include <vector>
 
 struct Color3
 {
@@ -20,17 +21,26 @@ struct Color3
 	}
 };
 
-void RecurseFillerInternal(Image* InImage, int InX, int InY, Color3 InColor, Color3 InSample)
-{
+bool IsVisitable(std::vector<bool>& InCache, int InX, int InY, Image* InImage) {
 	if (InImage->Height < InY || InY < 0)
-		return;
+		return false;
 	if (InImage->Width < InX || InX < 0)
-		return;
+		return false;
 
-	int Position = (InY * InImage->Width + InX) * 3;
+	int Index = (InY * InImage->Width + InX);
+	if (InCache[Index]) return false;
+	return true;
+}
+
+void RecurseFillerInternal(Image* InImage, int InX, int InY, Color3 InColor, Color3 InSample, std::vector<bool>& InCache)
+{
+	int Index = (InY * InImage->Width + InX);
+	int Position = Index * 3;
 
 	uint8_t* PointerToCurrentPixel = &InImage->Data[Position];
 	Color3* PointerToCurrentColor = (Color3*)PointerToCurrentPixel;
+
+	InCache[Index] = true;
 
 	/*
 	Color3 CurrentColor;
@@ -56,14 +66,33 @@ void RecurseFillerInternal(Image* InImage, int InX, int InY, Color3 InColor, Col
 	PointerToCurrentColor->G = InColor.G;
 	PointerToCurrentColor->B = InColor.B;
 
-	RecurseFillerInternal(InImage, InX + 1, InY, InColor, InSample);
-	RecurseFillerInternal(InImage, InX - 1, InY, InColor, InSample);
-	RecurseFillerInternal(InImage, InX, InY + 1, InColor, InSample);
-	RecurseFillerInternal(InImage, InX, InY - 1, InColor, InSample);
+	//Right Neighb
+	if (IsVisitable(InCache, InX + 1, InY, InImage)) {
+		RecurseFillerInternal(InImage, InX + 1, InY, InColor, InSample, InCache);
+	}
+	
+	//Left Neighb
+	if (IsVisitable(InCache, InX - 1, InY, InImage)) {
+		RecurseFillerInternal(InImage, InX - 1, InY, InColor, InSample, InCache);
+	}
+
+	//Bott Neighb
+	if (IsVisitable(InCache, InX, InY + 1, InImage)) {
+		RecurseFillerInternal(InImage, InX, InY + 1, InColor, InSample, InCache);
+	}
+
+	//Top Neighb
+	if (IsVisitable(InCache, InX, InY - 1, InImage)) {
+		RecurseFillerInternal(InImage, InX, InY - 1, InColor, InSample, InCache);
+	}
 }
 
 void RecursiveFiller(Image* InImage, int InX, int InY, Color3 InColor)
 {
+	auto Size = InImage->Width * InImage->Height;
+
+	std::vector<bool> Cache(Size, false);
+
 	int Position = (InY * InImage->Width + InX) * 3;
 
 	Color3 Sample;
@@ -71,7 +100,7 @@ void RecursiveFiller(Image* InImage, int InX, int InY, Color3 InColor)
 	Sample.G = InImage->Data[Position + 1];
 	Sample.B = InImage->Data[Position + 2];
 
-	RecurseFillerInternal(InImage, InX, InY, InColor, Sample);
+	RecurseFillerInternal(InImage, InX, InY, InColor, Sample, Cache);
 }
 
 int main()
@@ -98,4 +127,7 @@ int main()
 	delete MyImage;
 
 
+
+	//char = 1byte  vs char* = 8 byte
+	//sizeof(char) < sizeof(char*)
 }
